d2af5ba7b15f0a25cbaae2ee87765f631cb9c667
koudaiishigame <kira5052@gmail.com>
2020-11-12 00:12:28 +0900

    url編集

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,17 +16,17 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta name="description" content="This is website for photographers which are post and challenge international photo competitions." />
-          <link rel="canonical" href="https://international-photos-git-main.koudaiishigame.vercel.app/" />
+          <link rel="canonical" href="https://international-photos.vercel.app/" />
           <meta property="og:title" content="international photos" />
           <meta property="og:type" content="website" />
           <meta property="og:locale" content="ja_JP" />
-          <meta property="og:url" content="https://international-photos-git-main.koudaiishigame.vercel.app/" />
+          <meta property="og:url" content="https://international-photos.vercel.app/" />
           <meta property="og:image" content={card} />
           <meta name="twitter:title" content="international photos" />
           <meta name="twitter:card" content={card} />
           <meta name="twitter:description" content="This is website for photographers which are post and challenge international photo competitions." />
           <meta name="twitter:image" content={card} />
-          <link rel="dns-prefetch" href="https://international-photos-git-main.koudaiishigame.vercel.app/" />
+          <link rel="dns-prefetch" href="https://international-photos.vercel.app/" />
         </Head>
         <body style={{ margin: 0 }}>
           <Main />
