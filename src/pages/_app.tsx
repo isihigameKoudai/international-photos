6bd0730f9ca0ae53812c65155ad6cb49012f0cd3
koudaiishigame <kira5052@gmail.com>
2020-11-14 18:35:26 +0900

    static image file serve

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,33 @@
 import * as React from 'react'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import { useRouter } from 'next/router'
+import * as gtag from '../service/gtags.ts';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  React.useEffect(() => {
+    console.log("useEffect");
+
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  return (
+    <>
+      <Head>
+        <title>international photos</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
