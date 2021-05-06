0b696ca6723113321763769a88ddf04a4ba6f96e
koudaiishigame <kira5052@gmail.com>
2021-05-07 01:08:45 +0900

    feat: api追加

---
+++ src/utils/api.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosInstance } from "axios";
+
+const BASE_URL: string = process.env.BASE_URL;
+const API_KEY: string = process.env.API_KEY || "";
+const isProd: boolean = process.env.NODE_ENV;
+
+const headers = {
+  "Content-type": "application/json",
+  "Access-Control-Allow-Origin": isProd ? BASE_URL : "*",
+  // "Access-Control-Allow-Origin": "*",
+  "X-API-KEY": API_KEY,
+} as const;
+
+export const api: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers,
+});
