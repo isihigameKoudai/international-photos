d824512263be9d1816776a47dfc3a254a9bf786e
koudaiishigame <kira5052@gmail.com>
2022-05-13 18:37:46 +0900

    add clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,12 @@ class MyDocument extends Document {
             gtag('config', '${GOOGLE_ANALYTICS_ID}', {
               page_path: window.location.pathname,
             });
+
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "bvp5nosk2s");
           `,
             }}
           />
